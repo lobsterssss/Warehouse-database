007bc8197b46576bf1e7d6667a531ace339650b5
Lobster <92152762+lobsterssss@users.noreply.github.com>
2025-06-11 09:23:36 +0200

    update sql

--- database/Without_drop_warehouse_database.sql
+++ database/Without_drop_warehouse_database.sql
@@ -22,18 +22,14 @@ USE `warehouse`;
 -- Dumping structure for table warehouse.deliveries
 CREATE TABLE IF NOT EXISTS `deliveries` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Truck_ID` int(11) NOT NULL,
   `Store_ID` int(11) NOT NULL,
-  `Amount` int(11) DEFAULT NULL,
   `Status` enum('Deliverd','On_the_way','Being_prepared') DEFAULT 'Being_prepared',
   PRIMARY KEY (`ID`) USING BTREE,
-  KEY `Truck` (`Truck_ID`) USING BTREE,
   KEY `Store` (`Store_ID`),
-  CONSTRAINT `Store` FOREIGN KEY (`Store_ID`) REFERENCES `stores` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Truck` FOREIGN KEY (`Truck_ID`) REFERENCES `trucks` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `Store` FOREIGN KEY (`Store_ID`) REFERENCES `stores` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.deliveries: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.delivery_products
 CREATE TABLE IF NOT EXISTS `delivery_products` (
@@ -48,32 +44,23 @@ CREATE TABLE IF NOT EXISTS `delivery_products` (
   CONSTRAINT `Product` FOREIGN KEY (`Product_ID`) REFERENCES `products` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.delivery_products: ~0 rows (approximately)
-
--- Dumping structure for table warehouse.logs
-CREATE TABLE IF NOT EXISTS `logs` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Log` text DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table warehouse.logs: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.products
 CREATE TABLE IF NOT EXISTS `products` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Product_code` varchar(255) DEFAULT NULL,
-  `Product_info_ID` int(11) NOT NULL,
+  `Product_type_ID` int(11) NOT NULL,
   `Shelve_ID` int(11) NOT NULL,
   `Amount` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE,
+  KEY `Product_Type` (`Product_type_ID`) USING BTREE,
   KEY `Shelve` (`Shelve_ID`),
-  KEY `Product_Type` (`Product_info_ID`),
-  CONSTRAINT `Product_Type` FOREIGN KEY (`Product_info_ID`) REFERENCES `product_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Shelve` FOREIGN KEY (`Shelve_ID`) REFERENCES `shelves` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `Product_Type` FOREIGN KEY (`Product_type_ID`) REFERENCES `product_type` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Shelve` FOREIGN KEY (`Shelve_ID`) REFERENCES `shelves` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.products: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.product_type
 CREATE TABLE IF NOT EXISTS `product_type` (
@@ -81,59 +68,30 @@ CREATE TABLE IF NOT EXISTS `product_type` (
   `Name` varchar(255) NOT NULL,
   `Description` text DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.product_type: ~0 rows (approximately)
-
--- Dumping structure for table warehouse.reports
-CREATE TABLE IF NOT EXISTS `reports` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Report_Type_ID` int(11) NOT NULL,
-  `User_ID` int(11) DEFAULT NULL,
-  `Product_ID` int(11) DEFAULT NULL,
-  `Report` text DEFAULT NULL,
-  `ReportedAt` datetime DEFAULT NULL,
-  `HappendAt` datetime DEFAULT NULL,
-  `Status` enum('In behandeling','Opgelost') DEFAULT 'In behandeling',
-  PRIMARY KEY (`ID`),
-  KEY `Report_type` (`Report_Type_ID`),
-  KEY `Users_` (`User_ID`),
-  KEY `Products_` (`Product_ID`),
-  CONSTRAINT `Products_` FOREIGN KEY (`Product_ID`) REFERENCES `products` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Report_type` FOREIGN KEY (`Report_Type_ID`) REFERENCES `report_types` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `Users_` FOREIGN KEY (`User_ID`) REFERENCES `users` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table warehouse.reports: ~0 rows (approximately)
-
--- Dumping structure for table warehouse.report_types
-CREATE TABLE IF NOT EXISTS `report_types` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table warehouse.report_types: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.roles: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.shelves
 CREATE TABLE IF NOT EXISTS `shelves` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
   `Warehouse_ID` int(11) DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE,
   KEY `warehouse` (`Warehouse_ID`) USING BTREE,
-  CONSTRAINT `Warehouse` FOREIGN KEY (`Warehouse_ID`) REFERENCES `warehouses` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `Warehouse` FOREIGN KEY (`Warehouse_ID`) REFERENCES `warehouses` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.shelves: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.stores
 CREATE TABLE IF NOT EXISTS `stores` (
@@ -144,18 +102,7 @@ CREATE TABLE IF NOT EXISTS `stores` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.stores: ~0 rows (approximately)
-
--- Dumping structure for table warehouse.trucks
-CREATE TABLE IF NOT EXISTS `trucks` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `User_ID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`) USING BTREE,
-  KEY `Driver` (`User_ID`),
-  CONSTRAINT `Driver` FOREIGN KEY (`User_ID`) REFERENCES `users` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Dumping data for table warehouse.trucks: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -166,9 +113,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`ID`),
   KEY `Role` (`Role_ID`) USING BTREE,
   CONSTRAINT `Role` FOREIGN KEY (`Role_ID`) REFERENCES `roles` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.users: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.user_warehouses
 CREATE TABLE IF NOT EXISTS `user_warehouses` (
@@ -180,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `user_warehouses` (
   CONSTRAINT `Warehouse_ID` FOREIGN KEY (`Warehouse_ID`) REFERENCES `warehouses` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.user_warehouses: ~0 rows (approximately)
+-- Data exporting was unselected.
 
 -- Dumping structure for table warehouse.warehouses
 CREATE TABLE IF NOT EXISTS `warehouses` (
@@ -189,11 +136,9 @@ CREATE TABLE IF NOT EXISTS `warehouses` (
   `Postcode` varchar(255) DEFAULT NULL,
   `Street` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Dumping data for table warehouse.warehouses: ~1 rows (approximately)
-INSERT INTO `warehouses` (`ID`, `Name`, `Postcode`, `Street`) VALUES
-	(1, NULL, NULL, NULL);
+-- Data exporting was unselected.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
